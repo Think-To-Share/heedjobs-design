c36812190cd8714a925c285b3eaad2754c5aea0a
Tulika Das <tulikadas1029@gmail.com>
2021-10-06 16:01:51 +0530

    designed modified by tulika

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -14,3 +14,18 @@ if(document.querySelector(".industries-sec")){
         }
     })
 }
+if(document.querySelector(".success-section")){
+    tns({
+        container:".success-section .slider-container",
+        items:2,
+        nav:false,
+        controls:false,
+        responsive: {
+            768:{
+              items:5,
+              controlsText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+              controls:true
+            }
+        }
+    })
+}
